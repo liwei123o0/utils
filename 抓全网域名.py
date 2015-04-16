9f1af3a5dad36076cd4b8422ffcd312135371925
liwei <877129310@qq.com>
2015-04-16 15:54:00 +0800

    ONE-0.5

--- 抓全网域名.py
+++ 抓全网域名.py
@@ -82,3 +82,96 @@ if __name__=="__main__":
     starts.getUrl(urls)
     cur.close()
     conn.close()
+'''
+多线程实现
+import MySQLdb
+import threading
+import urllib2
+import re
+import time
+
+conn= MySQLdb.connect(
+    host='localhost',
+    port = 3306,
+    user='root',
+    passwd='root',
+    db ='',
+    )
+cur = conn.cursor()
+
+class newGet(object):
+    def __init__(self,url):
+        self.url= url
+
+    def findMin(self):
+        #存入数据库以后，从数据库里面从id最小的开始获取，写入数据库。
+        print "isfind"
+        sql = u"select url from test.get_url  where is_geted = 0 order by id desc limit 10"
+        cur.execute(sql)
+        res1 = cur.fetchall()
+        #for i in range(10):
+             #res1[i][0]
+        return res1
+
+    #获取网页内容
+    def getHtml(self,url):
+
+        try:
+            page = urllib2.urlopen(url,timeout=15)
+        except:
+            url = "http://www.yilongnews.com"
+            page = urllib2.urlopen(url)
+        print url
+        html = page.read()
+        return html
+
+    #正则表达式，获取url
+    def getImg(self,html):
+        reg = r'[a-zA-z]+:\/\/www?\.[0-9a-zA-z_]+[\.a-z]+'
+        imgre = re.compile(reg)
+        imglist = re.findall(imgre,html)
+        return imglist
+    def run(self):
+        urls = n.findMin()
+        for url in urls:
+            geth = n.getHtml(url[0])
+            geti = n.getImg(geth)
+            geti = list(set(geti))
+            print geti
+""""
+        #获取url
+    def getUrl(self):
+        print self.url
+        if self.url == '':
+            self.url = "http://www.yilongnews.com"
+        try:
+            status=urllib2.urlopen(self.url,timeout=15).code
+
+        except:
+            print urls
+            status = 203                        #打不开获取不到 状态吗
+        if status == 200:
+            html = getHtml(urls)
+            cc = getImg(html)
+            cc = list(set(cc)) # qu chongfu
+            #print cc
+            #写入数据库
+            #for aa in cc:
+                #print aa
+                #isFind(aa)
+        else:
+            sql2 = u"update get_url set is_geted = '2' where url = '%s'" % (urls)
+            cur.execute(sql2)
+            conn.commit()
+        time.sleep(0.02)
+        return
+"""
+
+
+if __name__ == '__main__':
+    n = newGet("http://www.yilongnews.com")
+    for i in xrange(10):
+        t =threading.Thread(target=n.run())
+        t.setDaemon(True)
+        t.start()
+'''
